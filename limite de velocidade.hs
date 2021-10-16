be04427b04982be8cd5cd777fc49cae2ecc2f028
TarcisioAraujo7 <tarcisio.olv@gmail.com>
2021-10-16 13:09:20 -0300

    Funções CW

---
+++ limite de velocidade.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+import CodeWorld
+
+type Mundo = (X,Y,[Point], Inclinacao, Velocidade)
+type Velocidade = Double
+type X = Double 
+type Y = Double
+type Inclinacao = Double
+main = activityOf mundoInicial update visualizacao 
+
+mundoInicial :: Mundo
+mundoInicial = ( 0, 0,[(0,2.5),(-1,-1.5),(1,-1.5)], 0 , 0)
+
+update :: Event -> Mundo -> Mundo
+update (KeyPress "Left") (posx,posy,[(x),(y),(z)], i, v)  = (posx,posy,[(x),(y),(z)], i + (pi / 6), v)
+update (KeyPress "Right") (posx,posy,[(x),(y),(z)], i, v) = (posx,posy,[(x),(y),(z)], i + (11 * pi / 6), v) 
+update (KeyPress "Up") (posx,posy,[(x),(y),(z)], i, v) 
+  | v <= 8 = (posx,posy,[(x),(y),(z)], i, v + 1)
+  | otherwise = (posx,posy,[(x),(y),(z)], i, v)
+update (TimePassing t)  (posx,posy,[(x),(y),(z)], i, v) = ( posx - v * sin(i) * t, posy + v * cos(i) * t, [(x),(y),(z)], i, v) 
+update _ (posx,posy,[(x),(y),(z)], i, v) = (posx,posy,[(x),(y),(z)], i, v)
+
+visualizacao :: Mundo -> Picture
+visualizacao (posx,posy,[(x),(y),(z)], i, v) = translated posx posy (rotated i (solidPolygon [(x),(y),(z)]))
---
+++ parte basica.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+import CodeWorld
+
+type Mundo = (X,Y,[Point], Inclinacao, Velocidade)
+type Velocidade = Double
+type X = Double 
+type Y = Double
+type Inclinacao = Double
+main = activityOf mundoInicial update visualizacao 
+
+mundoInicial :: Mundo
+mundoInicial = ( 0, 0,[(0,2.5),(-1,-1.5),(1,-1.5)], 0 , 0)
+
+update :: Event -> Mundo -> Mundo
+update (KeyPress "Left") (posx,posy,[(x),(y),(z)], i, v)  = (posx,posy,[(x),(y),(z)], i + (pi / 6), v)
+update (KeyPress "Right") (posx,posy,[(x),(y),(z)], i, v) = (posx,posy,[(x),(y),(z)], i + (11 * pi / 6), v) 
+update (KeyPress "Up") (posx,posy,[(x),(y),(z)], i, v) = (posx,posy,[(x),(y),(z)], i, v + 1)
+update (TimePassing t)  (posx,posy,[(x),(y),(z)], i, v) = ( posx - v * sin(i) * t, posy + v * cos(i) * t, [(x),(y),(z)], i, v) 
+update _ (posx,posy,[(x),(y),(z)], i, v) = (posx,posy,[(x),(y),(z)], i, v)
+
+visualizacao :: Mundo -> Picture
+visualizacao (posx,posy,[(x),(y),(z)], i, v) = translated posx posy (rotated i (solidPolygon [(x),(y),(z)]))
